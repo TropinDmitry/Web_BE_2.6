79782115baad704b21d50a7399a6d00f6be37d09
TropinDmitry <85580920+TropinDmitry@users.noreply.github.com>
2022-05-18 22:21:53 +0700

    Initial commit

---
+++ Web_BE_2.6/Services/IUserPermissionsService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Web_BE_6.Models;
+
+namespace Web_BE_6.Services
+{
+    public interface IUserPermissionsService
+    {
+        Boolean CanEditPost(Post post);
+
+        Boolean CanEditPostComment(PostComment postComment);
+    }
+}
